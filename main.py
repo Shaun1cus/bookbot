cf50b8470c8758e86618c1a7e9639941b2e83afa
Shaun1cus <shaun.putnam@student.swic.edu>
2025-06-23 19:46:47 -0500

    ignore pycache

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def main():
     book = 'books/frankenstein.txt'
     num_words = get_word_count(book)
     print(f'{num_words} words found in the document')
-    num_characters = get_character_count(book)
+    # print dictionary with character counts
+    char_count = get_character_count(book)
+    print(f'Character counts: {char_count}')
 
 main()
